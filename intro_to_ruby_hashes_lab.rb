4447a0b18966ea577fec55af5bd418bcb308f2d8
Willem A Duiker <duikhead2@gmail.com>
2020-08-21 18:33:17 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -35,5 +35,13 @@ def monopoly_with_fourth_tier
   # you built in the previous method. Run the tests. They will fail, but
   # they'll guide you in how to modify what you just did to have more
   # complexity.
+  hash = monopoly_with_third_tier
+  hash[:railroads][:names][:reading_railroad] = {mortgage_value: 100}
+  
+  
+  
+  hash
+
+
 
 end
