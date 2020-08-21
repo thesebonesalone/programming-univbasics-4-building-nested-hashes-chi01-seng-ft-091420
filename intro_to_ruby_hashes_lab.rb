9cf68764f08704de756521683e72aedea48fee9f
Willem A Duiker <duikhead2@gmail.com>
2020-08-21 18:30:18 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -12,7 +12,7 @@ def monopoly_with_second_tier
   # they'll guide you in how to modify what you just did to have more
   # complexity.
   hash = base_hash
-  hash[:railroads] = {pieces: 4, rent_in_dollars: {}, names: {}}
+  hash[:railroads] = {:pieces => 4, :rent_in_dollars => {}, :names => {}}
 
 end
 
